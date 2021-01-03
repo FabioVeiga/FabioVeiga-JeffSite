118cca026b9d650fa0ce36fe72dd806fae2df010
FabioVeiga <droidbinho@gmail.com>
2021-01-03 19:16:47 -0300

    Model criada

---
+++ JeffSite/Models/Carousel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JeffSite.Models
+{
+    public class Carousel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        
+        public Carousel(){}
+
+        public Carousel(int id, string title, string description, string link, DateTime expirationDate){
+            Id = id;
+            Title = title;
+            Description = description;
+            Link = link;
+            ExpirationDate = expirationDate;
+        }
+        
+    }
+}
