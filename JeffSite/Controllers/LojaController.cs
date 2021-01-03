7e0d7b7ccf93240bd8ef4160570662231cbd3300
FabioVeiga <droidbinho@gmail.com>
2021-01-03 10:09:42 -0300

    ajuste nas paginas leitor e loja

--- JeffSite/Controllers/LojaController.cs
+++ JeffSite/Controllers/LojaController.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JeffSite.Services;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace JeffSite.Controllers
 {
+    
+    
     public class LojaController : Controller
     {
+        private readonly SocialMidiaService _socialMidia;
+        public LojaController(SocialMidiaService socialMidia)
+        {
+            _socialMidia = socialMidia;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
+            ViewBag.Redes = _socialMidia.FindAll();
             return View();
         }
     }
