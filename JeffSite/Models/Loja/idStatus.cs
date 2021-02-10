67d8fc1c9c63402cfc7c4cba7d62ce30cd191ce6
FabioVeiga <droidbinho@gmail.com>
2021-02-09 21:28:01 -0300

    descricao do status

--- JeffSite/Models/Loja/idStatus.cs
+++ JeffSite/Models/Loja/idStatus.cs
@@ -1,10 +1,15 @@
-using System;
+using System.ComponentModel;
 
 namespace JeffSite.Models{
     public enum Status
     {
+        [Description("Aguardando Link")]
         Aguardando_Link = 1,
+
+        [Description("Aguardando Pagamento")]
         Aguardando_Pagamento = 2,
+
+        [Description("Pago e Aguardando dedicatório")]
         Pago_e_Aguardando_Dedicatorio = 3,
         Enviado = 4
     }
