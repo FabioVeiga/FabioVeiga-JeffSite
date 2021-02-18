26bdda069c9d3e842ff93bbd59af5b2726c4c6a8
FabioVeiga <droidbinho@gmail.com>
2021-02-17 22:10:47 -0300

    Campos na view

--- JeffSite/Models/Email.cs
+++ JeffSite/Models/Email.cs
@@ -1,13 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection.PortableExecutable;
 namespace JeffSite.Models
 {
     public class Email
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio!")]
         public string Servidor { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio!")]
         public int Porta { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio!")]
         public string ContaEmail { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio!")]
         public string Senha { get; set; }
         public bool HabilitaSSL { get; set; }
         
