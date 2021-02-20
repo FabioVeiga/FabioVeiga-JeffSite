7e4fc79e0c533f002a07472e041ffb9dde139010
FabioVeiga <droidbinho@gmail.com>
2021-02-20 18:31:10 -0300

    Add email do contato no malling

--- JeffSite/Models/Malling.cs
+++ JeffSite/Models/Malling.cs
@@ -7,6 +7,16 @@ namespace JeffSite.Models
         [Key]
         public string Email { get; set; }
         public string Nome { get; set; }
+
+        public Malling(){
+            
+        }
+
+        public Malling(string email, string nome)
+        {
+            Email = email;
+            Nome = nome;
+        }
         
     }
 }
--- JeffSite/Services/MallingService.cs
+++ JeffSite/Services/MallingService.cs
@@ -1,9 +1,30 @@
+using System.Linq;
 using JeffSite.Data;
+using JeffSite.Models;
 
 namespace JeffSite.Services
 {
     public class MallingService
     {
         private readonly JeffContext _context;
+
+         public MallingService(JeffContext context)
+        {
+            _context = context;
+        }
+
+        public void AddMalling(Malling mail){
+            _context.Add(mail);
+            _context.SaveChanges();
+        }
+
+        public bool CheckMail(Malling mail){
+            var teste = _context.Mallings.FirstOrDefault(x => x.Email == mail.Email);
+            if(teste == null){
+                return false;
+            }else{
+                return true;
+            }
+        }
     }
 }
