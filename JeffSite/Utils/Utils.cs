2ee3d88350fbb1e6bf9e5e43c2499742152ddcfa
FabioVeiga <droidbinho@gmail.com>
2021-03-06 13:00:09 -0300

    add site do correio no envio de email

--- JeffSite/Utils/Utils.cs
+++ JeffSite/Utils/Utils.cs
@@ -9,6 +9,8 @@ using JeffSite.Models;
 namespace JeffSite.Utils{
 
     public static class EnviarEmail{
+
+        private static string nomeSite = "Autor JP";
         
         private static string ModeloEmailContato(string namecontact, string phonecontact, string emailTo, string subject){
             return string.Format(@"<p> Nome: {0}</p>
@@ -25,21 +27,21 @@ namespace JeffSite.Utils{
                     background-color: #E1E1E1;"">
                         <tr>
                         <td>
-                            Jeff Autor
+                            {0}
                         </td>
                         </tr>
                         <tr>
                         <td style=""text-align: left;"">
-                            <p>Pedido: {0}</p>
-                            <p>Livro: {1}</p>
-                            <p>Nome: {2}</p>
-                            <p>Link de Pagamento: <a href=""{3}"">Clique aqui</a></p>
+                            <p>Pedido: {1}</p>
+                            <p>Livro: {2}</p>
+                            <p>Nome: {3}</p>
+                            <p>Link de Pagamento: <a href=""{4}"">Clique aqui</a></p>
                             <p>Obrigado por comprar nosso livro!</p>
                         </td>
                         </tr>
                     </table>
                     </html>"
-                , pedido, livro, nome, url); 
+                , nomeSite, pedido, livro, nome, url); 
         }
 
         private static string ModeloPedidoLinkRastreio(string nome, string livro, int pedido, string url){
@@ -50,22 +52,23 @@ namespace JeffSite.Utils{
                     background-color: #E1E1E1;"">
                         <tr>
                         <td>
-                            Jeff Autor
+                            {0}
                         </td>
                         </tr>
                         <tr>
                         <td style=""text-align: left;"">
                             <p>Obrigado pelo pagamento.</p><br>
-                            <p>Pedido: {0}</p>
-                            <p>Livro: {1}</p>
-                            <p>Nome: {2}</p>
-                            <p>Codigo de rastreio: {3}</a></p>
+                            <p>Pedido: {1}</p>
+                            <p>Livro: {2}</p>
+                            <p>Nome: {3}</p>
+                            <p>Código de rastreio: {4}</p>
+                            <p><a href='http://www.correios.com.br' target='_blank'>Correios</a></p>
                             <p>Obrigado por comprar nosso livro!</p>
                         </td>
                         </tr>
                     </table>
                     </html>"
-                , pedido, livro, nome, url); 
+                , nomeSite, pedido, livro, nome, url); 
         }
 
         public static bool testeEmail(Email config, string emailFrom, string emailTo, string subject, string nome, string phonecontact, string modelo, string livro, int? pedido, string url){
