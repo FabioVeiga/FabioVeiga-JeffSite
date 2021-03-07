520aa79822d77c115ba08d9fdc6f1d680468772f
FabioVeiga <droidbinho@gmail.com>
2021-03-07 19:26:17 -0300

    add url do site e nome do site

--- JeffSite/Models/Configuracao.cs
+++ JeffSite/Models/Configuracao.cs
@@ -19,6 +19,12 @@ namespace JeffSite.Models
 
         [Display(Name = "Endereco da loja do Mercado Livre")]
         public string UrlMercadoLivre { get; set; }
+        
+        [Display(Name = "Endereco do Site")]
+        public string UrlSite { get; set; }
+
+        [Display(Name = "Nome do Site")]
+        public string NomeSite { get; set; }
 
         public Configuracao()
         {
